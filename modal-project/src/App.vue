301675637ab3cb2398531fe10fd12f727ff511a9
pham.van.ngoc <pham.van.ngoc@suns-Mac-mini-2.local>
2022-09-27 16:29:17 +0700

    ls 23

---
+++ modal-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      title: 'My first vue app!!'
+    }
+  },
+  methods: {
+    handleClick () {
+      console.log(this.$refs.name.value)
+      this.$refs.name.classList.add('active')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
