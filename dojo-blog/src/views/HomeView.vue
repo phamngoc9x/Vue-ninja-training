861edb0d258502a0f8d52a47eec4e25ea737e749
phamngoc9x <phamvanngoc441991@gmail.com>
2022-10-09 13:51:34 +0700

    'dojo'

---
+++ dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1> Home</h1>
+    <post-list v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">Toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {ref} from 'vue'
+import PostList from '../components/PostList.vue'
+
+export default {
+  components: { PostList },
+  name: 'HomeView',
+  setup() {
+    const posts = ref([
+      {title: 'welcome to the blog', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', id: 1},
+      {title: 'Top 10 CSS tip', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', id: 2}
+    ])
+
+    const showPosts = ref(true)
+    return { posts, showPosts }
+  }
+}
+</script>
